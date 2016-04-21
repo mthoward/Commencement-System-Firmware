d0296648d27b231cf0364e90a55b8a25d52fe13b
Seth Karas <sethkara@buffalo.edu>
2016-04-21 18:43:55 -0400

    updates udp message handling

--- src/utils/UDP_Class.py
+++ src/utils/UDP_Class.py
@@ -1,7 +1,7 @@
 import socket
 import time
 import struct
-from multiprocessing import Process, Lock
+import threading
 import binascii
 '''
 sudo iptables -A INPUT -p udp --dport 15555 -j ACCEPT
@@ -9,13 +9,13 @@ sudo iptables -A INPUT -p udp --dport 15556 -j ACCEPT
 
 Types of Messages:
    UBIT_LOOKUP
-      Ask if UBIT Exists      (Scanner to Releaser)     <= MESS_ID = 0x0001
-      Respond if UBIT Exists  (Scanner to Releaser)     <= MESS_ID = 0x0002
+      Ask if UBIT Exists      (Scanner to Releaser)     <= MESS_ID   = 0x0001
+      Respond if UBIT Exists  (Scanner to Releaser)     <= MESS_ID   = 0x0002
          Yes                                            <= MESS_HDR1 = 0x0001
          No                                             <= MESS_HDR1 = 0x0000
-   Laser Broken            (Releaser to Scanner)        <= MESS_ID = 0x8000
-   Ask if Connected        (two-way)                    <= MESS_ID = 0x0100
-   Respond if Connected    (two-way)                    <= MESS_ID = 0x0200
+   Laser Broken            (Releaser to Scanner)        <= MESS_ID   = 0x8000
+   Ask if Connected        (two-way)                    <= MESS_ID   = 0x0100
+   Respond if Connected    (two-way)                    <= MESS_ID   = 0x0200
 '''
 
 class UDP_Class():
@@ -32,17 +32,23 @@ class UDP_Class():
                       # localPort = 15555,
                       # socketTimeout = 100):
    ### Releaser Pi
-   def __init__(self, other_Pi_IP = '169.254.199.241',
-                      other_Pi_Port = 15556,
-                      localIP = '169.254.104.90',
-                      localPort = 15555,
-                      socketTimeout = 100):
+   # def __init__(self, other_Pi_IP = '169.254.199.241',
+                      # other_Pi_Port = 15556,
+                      # localIP = '169.254.104.90',
+                      # localPort = 15555,
+                      # socketTimeout = 100):
    # ### Scanner Pi
    # def __init__(self, other_Pi_IP = '169.254.104.90',
                       # other_Pi_Port = 15555,
                       # localIP = '169.254.199.241',
                       # localPort = 15556,
                       # socketTimeout = 100):
+   ### Other
+   def __init__(self, other_Pi_IP = '172.20.10.6',
+                      other_Pi_Port = 15555,
+                      localIP = '172.20.10.3',
+                      localPort = 15556,
+                      socketTimeout = 100):
       # Set UDP port and IP
       self.OTHER_PI_IP_ADDRESS = other_Pi_IP
       self.OTHER_PI_PORT = other_Pi_Port
@@ -61,14 +67,15 @@ class UDP_Class():
       self.RESP_COMMS_UP_MESS_ID    = int("0200",16)
       
       # Message specific buffers and flags
-      self.CONNECTION = 0
-      self.RCV_UBIT_FLAG = 0
-      self.RCV_UBIT_BUFFER = ""
-      self.RECEIVED_UBIT = 0
-      self.RECEIVED_UBIT_BUFFER = ""
-      self.UBIT_INCOMING_BUFFER = ""
-      #self.RCV_UBIT_FLAG_LOCK = threading.Lock()
-      #self.RCV_UBIT_BUFFER_LOCK = threading.Lock()
+      self.CONNECTION            = 0
+      self.RCV_UBIT_FLAG         = 0
+      self.RCV_UBIT_BUFFER       = ""
+      self.RECEIVED_UBIT         = 0
+      self.RECEIVED_UBIT_BUFFER  = ""
+      self.UBIT_INCOMING_BUFFER  = ""
+      self.RESPONSE_TO_ASK_UBIT  = 0
+      self.ASK_UBIT_ANSWER       = 0
+      self.lock = threading.Lock()
     
       # Sets internal socket timeout
       self.socketTimeout = socketTimeout
@@ -88,7 +95,6 @@ class UDP_Class():
                         int("0000", 16)]
       
       self.NUM_ALL_HEADER_WORDS = len(self.UB_HEADER)
-
  
    def connectUDPSockets(self):
       ##### Open my socket ####
@@ -165,45 +171,50 @@ class UDP_Class():
                      for i in range(0,len(payload)):
                         hex = format(payload[i], '04X')
                         ubit = ubit + (str(hex.decode("hex")))
-                     self.RCV_UBIT_FLAG_LOCK.acquire()
+                     self.lock.acquire()
                      self.RCV_UBIT_FLAG = 1
-                     self.RCV_UBIT_BUFFER = message
-                     self.RCV_UBIT_FLAG_LOCK.release()
-                     ###TODO
-                     #Look up if name exists
-                  
-                  
+                     self.RCV_UBIT_BUFFER = ubit
+                     print ubit
+                     self.lock.release()
+                     self.sendMessage(payload="Yes", MESS_ID=self.RESP_UBIT_MESS_ID, MESS_HDR1=self.RESP_UBIT_HDR1_YES)
+                     
                   '''UBIT_RESPONSE_MESS'''
-                  if int((format(headerWords[5], '04X')),16) == self.UBIT_RESPONSE_MESS:  
-                     if int((format(headerWords[5], '04X')),16) == self.RESP_UBIT_HDR1_YES:
-                        # Confirm yes
-                        print yes
-                     if int((format(headerWords[5], '04X')),16) == self.RESP_UBIT_HDR1_NO:
-                        # Confirm No
-                        print no
+                  if int((format(headerWords[5], '04X')),16) == self.RESP_UBIT_MESS_ID:  
+                     self.lock.acquire()
+                     self.RESPONSE_TO_ASK_UBIT = 1
+                     self.lock.release()
+                     if int((format(headerWords[6], '04X')),16) == self.RESP_UBIT_HDR1_YES:
+                        self.ASK_UBIT_ANSWER = 1
+                        print "Exists"
+                     if int((format(headerWords[6], '04X')),16) == self.RESP_UBIT_HDR1_NO:
+                        self.ASK_UBIT_ANSWER = 0
+                        print "Doesn't Exist"
                   
                   '''ASK_COMMS_UP_MESS_ID'''
                   if int((format(headerWords[5], '04X')),16) == self.ASK_COMMS_UP_MESS_ID:
-                     self.sendMessage("I'm Alive", self.RESP_COMMS_UP_MESS_ID)
+                     self.sendMessage(payload="I'm Alive", MESS_ID=self.RESP_COMMS_UP_MESS_ID)
                   
                   '''RESP_COMMS_UP_MESS_ID'''
                   if int((format(headerWords[5], '04X')),16) == self.RESP_COMMS_UP_MESS_ID:
+                     self.lock.acquire()
                      self.CONNECTION = 1
-             
-                  '''RESP_COMMS_UP_MESS_ID'''
-                  if int((format(headerWords[5], '04X')),16) == self.RESP_COMMS_UP_MESS_ID:
-                     self.CONNECTION = 1
+                     self.lock.release()
                      
-                     
+                  '''LASER_BROKEN_MESS_ID'''
+                  if int((format(headerWords[5], '04X')),16) == self.LASER_BROKEN_MESS_ID:
+                     self.lock.acquire()
+                     self.LASER_BROKEN = 1
+                     self.lock.release()
+          
          except Exception as e:
-	   print e.message
-	   print e
-           print "timeout or error..."
+            print e.message
+            print e
+            print "timeout or error..."
    
    def checkConnection(self):
       while(1):
          self.CONNECTION = 0
-         self.sendMessage("Are you Alive", self.STATUS_REQUEST_MESS)
+         self.sendMessage("Are you Alive", self.ASK_COMMS_UP_MESS_ID)
          seconds = 0
          while(self.CONNECTION == 0):
             time.sleep(1.0)
@@ -233,3 +244,15 @@ class UDP_Class():
       self.statusThread = threading.Thread(target=self.checkConnection)
       self.statusThread.daemon = True
       self.statusThread.start()
+
+      
+#Debug      
+udp = UDP_Class()
+udp.listenForMessages()
+udp.check_Connection()
+x=0
+while(1):
+   time.sleep(0.1)
+   udp.sendMessage("test"+str(x), MESS_ID=udp.ASK_UBIT_MESS_ID)
+   x += 1
+   
